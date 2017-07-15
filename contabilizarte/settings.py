93685166c547b16592493c9d98083e819f652c5b
Bruno Barbosa <bruno.barbosa@corp.globo.com>
2017-07-14 23:22:43 -0300

    chore: add theme app

--- contabilizarte/settings.py
+++ contabilizarte/settings.py
@@ -232,6 +232,7 @@ if DJANGO_VERSION < (1, 9):
 ################
 
 INSTALLED_APPS = (
+    "contabilizarte.theme",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
