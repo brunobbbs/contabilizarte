8f01f05e95875e56df2474f8f067cae8b75664f6
Bruno Barbosa <bsbruno1@gmail.com>
2017-08-06 18:59:11 -0300

    theme/admin: improve admin visualization

--- contabilizarte/theme/admin.py
+++ contabilizarte/theme/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from contabilizarte.theme.models import SpecialCategory, ImportantLinks
 
 
-admin.site.register(SpecialCategory)
-admin.site.register(ImportantLinks)
+class SpecialCategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url', 'active')
+
+
+class ImportantLinksAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url', 'active')
+
+
+admin.site.register(SpecialCategory, SpecialCategoryAdmin)
+admin.site.register(ImportantLinks, ImportantLinksAdmin)
