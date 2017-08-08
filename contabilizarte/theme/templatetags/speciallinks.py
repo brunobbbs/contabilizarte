754211f62432401669f7ca882e043d3c0367703b
Bruno Barbosa <bsbruno1@gmail.com>
2017-08-07 21:16:40 -0300

    templatetags/speciallinks: implement category colors

--- contabilizarte/theme/templatetags/speciallinks.py
+++ contabilizarte/theme/templatetags/speciallinks.py
@@ -4,10 +4,26 @@ from contabilizarte.theme.models import SpecialCategory, ImportantLinks
 register = template.Library()
 
 
+CATEGORIES_COLOR_ORDERING = ['laranja', 'azul', 'verde', 'amarelo']
+
+
 @register.simple_tag
-def special_links(_type='important-links'):
-    if _type == 'category':
-        links = SpecialCategory.objects.filter(active=True)
-    else:
-        links = ImportantLinks.objects.filter(active=True)
-    return links
+def render_important_links():
+    return ImportantLinks.objects.filter(active=True)
+
+
+@register.simple_tag
+def render_special_categories():
+    categories = SpecialCategory.objects.filter(active=True)
+    colors = get_category_colors(categories.count())
+    return zip(categories, colors)
+
+
+def get_category_colors(max_value=1):
+    import itertools
+    qtd = 0
+    for color in itertools.cycle(CATEGORIES_COLOR_ORDERING):
+        if qtd >= int(max_value):
+            break
+        qtd += 1
+        yield color
--- contabilizarte/theme/tests/test_special_categories_color.py
+++ contabilizarte/theme/tests/test_special_categories_color.py
@@ -1,10 +1,5 @@
 import unittest
-from contabilizarte.theme.templatetags.speciallinks import (
-    CATEGORIES_COLOR_ORDERING,
-    gen_category_color,
-    next_color,
-    get_color,
-)
+from contabilizarte.theme.templatetags.speciallinks import CATEGORIES_COLOR_ORDERING
 
 
 class SpecialCategoriesColorTest(unittest.TestCase):
@@ -13,9 +8,3 @@ class SpecialCategoriesColorTest(unittest.TestCase):
             ['laranja', 'azul', 'verde', 'amarelo'],
             CATEGORIES_COLOR_ORDERING
         )
-
-    def test_colors_hex(self):
-        self.assertEqual('#f74c00', get_color('laranja'))
-        self.assertEqual('#46c9dd', get_color('azul'))
-        self.assertEqual('#3fe525', get_color('verde'))
-        self.assertEqual('#fff200', get_color('amarelo'))
